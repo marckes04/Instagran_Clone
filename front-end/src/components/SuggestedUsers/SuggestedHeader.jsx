2f862fa174cc0372becbf396c8a5d7c5b30bb28d
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2026-04-19 03:30:58 +0200

    Homepage Ready

--- front-end/src/components/SuggestedUsers/SuggestedHeader.jsx
+++ front-end/src/components/SuggestedUsers/SuggestedHeader.jsx
@@ -1,9 +1,29 @@
+import { Avatar, Flex ,Text,Link} from '@chakra-ui/react'
 import React from 'react'
+import { Link as RouterLink} from 'react-router-dom';
 
 const SuggestedHeader = () => {
   return (
-    <div>SuggestedHeader</div>
-  )
-}
+    <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
+        <Flex alignItems={"center"} gap={2}>
+            <Avatar name='As a Programmer' size={"lg"} src='profilepic.png'/>
+                <Text fontSize={12} fontWeight={"bold"}>
+                    asaprogrammer_
+                </Text>
+        </Flex>
+        <Link
+        as={RouterLink}
+        to={"/auth"}
+        fontSize={14}
+        fontWeight={"medium"}
+        color={"blue.400"}
+        style={{textDecoration:"none"}}
+        cursor={"pointer"}
+        >
+            Log out
+        </Link>
+    </Flex>
+  );
+};
 
 export default SuggestedHeader
--- front-end/src/components/SuggestedUsers/SuggestedUsers.jsx
+++ front-end/src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { Text, Flex, VStack, Box, Link as ChakraLink } from '@chakra-ui/react'; // Importamos Link de Chakra con un alias
+import React from 'react';
+import SuggestedHeader from './SuggestedHeader';
+import SuggestedUser from './SuggestedUser';
 
 const SuggestedUsers = () => {
   return (
-    <div>SuggestedUsers</div>
-  )
-}
+    <VStack py={8} px={6} gap={4}>
+      <SuggestedHeader />
 
-export default SuggestedUsers
+      <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+        <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+          Suggested for you
+        </Text>
+        <Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
+          See All
+        </Text>
+      </Flex>
+
+      {/* Lista de usuarios sugeridos */}
+      <SuggestedUser name='Dan Abramov' followers={1392} avatar='https://bit.ly/dan-abramov' />
+      <SuggestedUser name='Ryan Florence' followers={567} avatar='https://bit.ly/ryan-florence' />
+      <SuggestedUser name='Christian Nwamba' followers={759} avatar='https://bit.ly/code-beast' />
+
+      {/* Footer de la sección */}
+      <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
+        © 2026 Built By{" "}
+        <ChakraLink 
+          href="https://github.com/marckes04" 
+          target='_blank' 
+          color='blue.400' 
+          fontSize={12} 
+          style={{ textDecoration: "none" }}
+          _hover={{ color: "blue.300" }}
+        >
+          As a Programmer
+        </ChakraLink>
+      </Box>
+    </VStack>
+  );
+};
+
+export default SuggestedUsers;
