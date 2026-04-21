f9742b13979476766532159b73d23e4b7e7396f0
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2026-04-21 18:18:59 +0200

    prfile tabs completed

--- front-end/src/components/Profile/ProfileTabs.jsx
+++ front-end/src/components/Profile/ProfileTabs.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { Box, Flex, Text } from '@chakra-ui/react'; // 1. Añadimos Text aquí
+import { BsBookmark, BsGrid3X3, BsSuitHeart } from 'react-icons/bs'; // 2. X mayúscula
+import React from 'react';
 
 const ProfileTabs = () => {
   return (
-    <div>ProfileTabs</div>
-  )
-}
+    <Flex
+      w={"full"}
+      justifyContent={"center"}
+      gap={{ base: 4, sm: 10 }}
+      textTransform={"uppercase"}
+      fontWeight={"bold"}
+    >
+      {/* TAB 1: POSTS */}
+      <Flex borderTop={"1px solid white"} alignItems={"center"} p="3" gap={1} cursor={"pointer"}>
+        <Box fontSize={20}>
+          <BsGrid3X3 /> {/* 3. Nombre exacto y etiqueta cerrada */}
+        </Box>
+        <Text fontSize={12} display={{ base: "none", sm: "block" }}>Posts</Text>
+      </Flex>
 
-export default ProfileTabs
+      {/* TAB 2 */}
+      <Flex  alignItems={"center"} p="3" gap={1} cursor={"pointer"}>
+        <Box fontSize={20}>
+          <BsBookmark/>
+        </Box>
+        <Text fontSize={12} display={{ base: "none", sm: "block" }}>Saved</Text>
+      </Flex>
+      
+      {/* TAB 3 */}
+      <Flex  alignItems={"center"} p="3" gap={1} cursor={"pointer"}>
+        <Box fontSize={20}>
+          <BsSuitHeart />
+        </Box>
+        <Text fontSize={12} display={{ base: "none", sm: "block" }}>Posts</Text>
+      </Flex>
+
+    </Flex>
+  );
+};
+
+export default ProfileTabs;
