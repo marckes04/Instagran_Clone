30084f3c7f4cc8c3f87c2d71340c5ab9cc1d6938
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2026-04-21 17:50:05 +0200

    prfile header completed

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
 import Authpage from "./pages/Authpage/Authpage";
 import PageLayout from "./Layout/PageLayout/PageLayout";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element ={<Homepage/>}/>
         <Route path='/auth' element ={<Authpage/>}/>
+        <Route path='/:username' element ={<ProfilePage/>}/>
       </Routes>
     </PageLayout>
   );
